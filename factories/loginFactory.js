f781612598a59837bef44431ee44cd3484f81ad7
Sarah Monks <monks.sarah@gmail.com>
2017-10-06 14:29:50 +0100

    Add files via upload

--- factories/loginFactory.js
+++ factories/loginFactory.js
@@ -7,7 +7,7 @@ mapApp.factory('loginFactory', function($http, $timeout, $q, sharedFactory){
         $http({
             method: 'POST',
             //url: 'http://localhost/API/facebookAuth?apiKey=1a0bca66-82af-475a-8585-90bc0417883d',
-           url: 'http://gamuzic.com/API/auth?apiKey=1a0bca66-82af-475a-8585-90bc0417883d',
+           url: 'http://gamuzic.com/API/facebookauth?apiKey=1a0bca66-82af-475a-8585-90bc0417883d',
             data: JSON.stringify(data),
             headers: {
                'Content-Type': 'application/json;charset=utf-8'
