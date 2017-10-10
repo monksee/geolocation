5be0d34429237d2016fef13eaafa990ec1645ac9
Sarah Monks <monks.sarah@gmail.com>
2017-10-10 13:54:15 +0100

    Add files via upload

--- factories/localStorageFactory.js
+++ factories/localStorageFactory.js
@@ -1,7 +1,7 @@
 /*
  * A factory that makes calls to PhoneGap's geolocation API.
  */
-mapApp.factory('localStorageFactory', function (phonegapReady) {
+mapApp.factory('localStorageFactory', function (phonegapReady, loginFactory) {
     return {
         //we want to wrap our implementation in our phonegapReady function in case deviceready has not fired yet.
         //If deviceready has not fired then this call will be put in a queue until it fires.
@@ -19,6 +19,7 @@ mapApp.factory('localStorageFactory', function (phonegapReady) {
               "userToken" : localStorage.getItem("userToken")
                // "userToken" : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySUQiOjMxLCJmYWNlYm9va1VzZXJJRCI6IjEwMjEzNzE4NTUyNjE0MzI2In0.SdWuJQ8uvAt4neH6Pxr0zzh_TRB5un2rKWYQHfo2fpo"
             };
+            alert('data user token ' + data.userToken);
             loginFactory.checkUserToken(data).then(function(userDetails) {
               //Since the checkUserToken method (in the loginFactory) is performaing a http request we need to use a promise
               //to store the userDetails (from the response) into our $scope.userDetails variable. 
