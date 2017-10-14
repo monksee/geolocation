5c4050ceeab37f923b4c13f0886225dfccb11f12
Sarah Monks <monks.sarah@gmail.com>
2017-10-14 13:58:12 +0100

    Add files via upload

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,11 @@
 //configure our routes
 mapApp.config(function($routeProvider) {
 	$routeProvider
+	    .when('/login', {
+			//route for the home page
+			templateUrl : 'views/login.html',
+			controller : 'loginController'
+		})
 		.when('/', {
 			//route for the home page
 			templateUrl : 'views/home.html',
