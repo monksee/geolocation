835a591f2566c8844f66a85e0399951ac8d7c044
Sarah Monks <monks.sarah@gmail.com>
2017-11-01 14:22:28 +0000

    Add files via upload

--- factories/stationFactory.js
+++ factories/stationFactory.js
@@ -451,12 +451,13 @@ mapApp.factory('stationFactory', function($http, $timeout, $q, sharedFactory, us
          * We use this in our main controller after we have detected that the home view has finished loading.
          */
        //  var allStationsMapData = [{stationLatLng: {lat: 53.41291, lng: -8.24389}, stationID: "5"},{stationLatLng: {lat: 53.3498053, lng: -6.2603097}, stationID: "5"}];
-        var allStationsMapDat = [{"stationID":1,"stationName":"Finn's Maxol ","stationLatLng":{"lat":53.56077194,"lng":-7.76224327}},
+        var allStationsMapDa = [{"stationID":1,"stationName":"Finn's Maxol ","stationLatLng":{"lat":53.56077194,"lng":-7.76224327}},
         {"stationID":2,"stationName":"Texaco Spar Foxrock ","stationLatLng":{"lat":53.26651001,"lng":-6.18408442}},
         {"stationID":3,"stationName":"Texaco Spar Enniskerry","stationLatLng":{"lat":53.20385361,"lng":-6.17581844}}];
 
 
          alert("map" + document.getElementById('map'));
+           alert("map data length " + allStationsMapData.length);
          alert("map data" + JSON.stringify(allStationsMapData));
         var map = new google.maps.Map(document.getElementById('map'),{
             zoom: 9,
