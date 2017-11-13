129f2fc70a419a6fc486765e4a234d58cf69a334
Sarah Monks <monks.sarah@gmail.com>
2017-11-13 18:17:13 +0000

    Add files via upload

--- factories/geolocationFactory.js
+++ factories/geolocationFactory.js
@@ -5,7 +5,7 @@ mapApp.factory('geolocationFactory', function ($rootScope, phonegapReady) {
     return {
         //we want to wrap our implementation in our phonegapReady function in case deviceready has not fired yet.
         //If deviceready has not fired then this call will be put in a queue until it fires.
-        getCurrentPosition: phonegapReady(function (onSuccess, onError, options) {
+        getCurrPosition: phonegapReady(function (onSuccess, onError, options) {
            
             navigator.geolocation.getCurrentPosition(function () {
                  alert('success geolocation factory');
